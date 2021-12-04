9143270f7709aa8e5e4bc0f3450b1ce2c0ce42e8
Sten Seifert <sten.seifert@googlemail.com>
2021-12-04 14:36:54 +0100

    Day 3 - Part 2

--- src/main/java/de/startat/aoc2021/solutions/ThirdDay.java
+++ src/main/java/de/startat/aoc2021/solutions/ThirdDay.java
@@ -5,8 +5,10 @@ import org.apache.logging.log4j.util.Strings;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.function.BiFunction;
 import java.util.stream.Collectors;
 
 import static java.util.stream.Collectors.toList;
@@ -21,11 +23,11 @@ public class ThirdDay {
     public void run() throws Exception{
         List<String> lines = fileReadService.getFileLines("ThirdDay_Data1.txt");
         int bitLength = lines.get(0).length();
-        List<List<Integer>> bits = lines.stream().map(s -> Arrays.stream(s.split("")).map(Integer::valueOf).collect(toList())).collect(toList());
+        List<List<Integer>> bitList = lines.stream().map(s -> Arrays.stream(s.split("")).map(Integer::valueOf).collect(toList())).collect(toList());
 
         log.info(()->"amount of bits per line:" + bitLength);
 
-        String gammaRate = buildGammaRate(bits, bitLength);
+        String gammaRate = buildGammaRate(bitList);
 
         log.info(()->"gamma rate: " + gammaRate);
 
@@ -39,19 +41,68 @@ public class ThirdDay {
         log.info(()->"gamma rate numeric: " + gammaRateNumeric);
         log.info(()->"epsilon rate numeric: " + epsilonRateNumeric);
         log.info(()->"gamma x epsilon: " + (gammaRateNumeric * epsilonRateNumeric));
+
+        /*
+            Solution to part 1
+            --------------------------
+            amount of bits per line:12
+            gamma rate: 111011110011
+            least common bits: 000100001100
+            gamma rate numeric: 3827
+            epsilon rate numeric: 268
+            gamma x epsilon: 1025636
+        */
+
+        String oxygenGeneratorRating = findRatingForSecondStar(copyBitList(bitList), this::findMostCommonBit);
+        log.info("oxygen generator rating: " + oxygenGeneratorRating);
+        log.info("oxygen generator rating: " + bitStringToNumeric(oxygenGeneratorRating));
+        String co2ScrubberRating = findRatingForSecondStar(copyBitList(bitList), (a, b) -> 1 - findMostCommonBit(a,b));
+        log.info("CO2 scrubber rating: " + co2ScrubberRating);
+        log.info("CO2 scrubber rating: " + bitStringToNumeric(co2ScrubberRating));
+        log.info("life support rating: " + bitStringToNumeric(oxygenGeneratorRating) * bitStringToNumeric(co2ScrubberRating));
+
+        /*
+            Solution to part 2
+            --------------------------
+            oxygen generator rating: 110000010001
+            oxygen generator rating: 3089
+            CO2 scrubber rating: 000100000001
+            CO2 scrubber rating: 257
+            life support rating: 793873
+         */
     }
 
-    private String buildGammaRate(List<List<Integer>> bits, int bitLength){
-        StringBuilder result = new StringBuilder(Strings.repeat("0",bitLength));
-        for(int i = 0; i<bitLength;i++){
-            final int index = i;
-            long zeroCountForCurrentBit = bits.stream().map(v -> v.get(index)).filter(v -> v.equals(0)).count();
-            if(zeroCountForCurrentBit < bits.size()/2){
-                result.setCharAt(index,'1');
-            }
+    private String findRatingForSecondStar(List<List<Integer>> bitList, BiFunction<List<List<Integer>>,Integer, Integer> filterBitValueFunction ) {
+        int index = 0;
+        while(bitList.size() > 1){
+            Integer filterBitValue = filterBitValueFunction.apply(bitList,index);
+            final int finalIndex = index;
+            bitList = bitList.stream().filter(v -> v.get(finalIndex).equals(filterBitValue)).collect(Collectors.toList());
+            index++;
+
+        }
+        return bitList.stream().flatMap(v -> v.stream().map(String::valueOf)).collect(Collectors.joining());
+    }
+
+    private List<List<Integer>> copyBitList(List<List<Integer>> bitList) {
+        return bitList.stream().map(ArrayList::new).collect(toList());
+    }
+
+    private String buildGammaRate(List<List<Integer>> bits/*, int bitLength*/){
+        int bitStringLength = bits.get(0).size();
+        StringBuilder result = new StringBuilder(Strings.repeat("0",bitStringLength));
+        for(int i = 0; i< bitStringLength; i++){
+            int mostCommonBitAtIndex = findMostCommonBit(bits,i);
+            result.setCharAt(i, Character.forDigit(mostCommonBitAtIndex,10));
         }
         return result.toString();
     }
+
+    private int findMostCommonBit(List<List<Integer>> bitList, int index){
+        long zeroCountForCurrentBit = bitList.stream().map(v -> v.get(index)).filter(v -> v.equals(0)).count();
+        return zeroCountForCurrentBit > bitList.size()/2 ? 0 : 1;
+    }
+
 
     private String invertBitString(String bitString){
         return Arrays.stream(bitString.split("")).map(v -> v.equals("0")?"1":"0").collect(Collectors.joining());
